e20178878550cfe804df13d58adb1a92b52e0f5d
Robyn Klopp <rklopp@gmail.com>
2018-08-08 21:43:16 -0700

    add URI Builder and settings update

--- app/src/main/java/com/robynandcory/goodnewseveryone/NewsListActivity.java
+++ app/src/main/java/com/robynandcory/goodnewseveryone/NewsListActivity.java
@@ -3,38 +3,44 @@ package com.robynandcory.goodnewseveryone;
 import android.content.Context;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v4.content.CursorLoader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-
 
 
 /**
  * Today in Football is a news app for Udacity ABND
  * that loads the latest Football stories from the Guardian API.
- *
+ * <p>
  * With thanks to references:
  * For JSON parsing and networking reference:
  * https://github.com/udacity/ud843-QuakeReport
  * https://github.com/doowtnehpets/BitBacklashNewsfeed
- *
+ * https://medium.com/@JakobUlbrich/building-a-settings-screen-for-android-part-1-5959aa49337c
+ * <p>
  * for RecyclerView with CardView assistance:
  * https://medium.com/@droidbyme/android-recyclerview-fca74609725e
  * Free Stock photo from http://absfreepic.com/
@@ -45,10 +51,14 @@ public class NewsListActivity extends AppCompatActivity
 
     //URL for data from the Guardian API
     private static final String stringURL =
-            "https://content.guardianapis.com/search?show-fields=thumbnail%2Cbyline&section=football&api-key=91b606b8-a998-4f76-9e92-5faf5f08fdb5";
-
+            "https://content.guardianapis.com/search?show-fields=thumbnail%2Cbyline";
+    //API Key for Guardian endpoint
+    private static final String apiKey = "91b606b8-a998-4f76-9e92-5faf5f08fdb5";
     //Loader ID
     private static final int NEWS_LOADER_ID = 0;
+
+    //default value for setting limiting stories to a single day
+    private Boolean defaultLimitDate = false;
 
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView newsView;
@@ -64,11 +74,11 @@ public class NewsListActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
 
-        //find the RecyclerView and set the layoutmanager
+        //find the RecyclerView and set the layoutManager
         newsView = findViewById(R.id.news_recycler);
         newsView.setLayoutManager(new LinearLayoutManager(this));
 
-        //find the SwipreRefreshLayout
+        //find the SwipeRefreshLayout
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_recycler);
         swipeRefreshLayout.setRefreshing(true);
 
@@ -106,7 +116,7 @@ public class NewsListActivity extends AppCompatActivity
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()){
+        if (networkInfo != null && networkInfo.isConnected()) {
             return true;
         } else {
             return false;
@@ -125,7 +135,38 @@ public class NewsListActivity extends AppCompatActivity
     @NonNull
     @Override
     public Loader<List<NewsItem>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this, stringURL);
+//            "https://content.guardianapis.com/search?show-fields=thumbnail%2Cbyline&section=football&page-size=25&api-key=91b606b8-a998-4f76-9e92-5faf5f08fdb5"
+//            "https://content.guardianapis.com/search?show-fields=thumbnail%252Cbyline&section=football&page-size=10&api-key=91b606b8-a998-4f76-9e92-5faf5f08fdb5";
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String minStories = sharedPreferences.getString(
+                getString(R.string.settings_number_of_stories_key),
+                getString(R.string.settings_number_of_stories_default));
+        Boolean limitToToday = sharedPreferences.getBoolean(
+                getString(R.string.settings_date_of_stories_key),
+                defaultLimitDate);
+
+        //parse string URL base from Guardian
+        Uri uriStart = Uri.parse(stringURL);
+        Uri.Builder uriBuilder = uriStart.buildUpon();
+        uriBuilder.appendQueryParameter("section", "football");
+        uriBuilder.appendQueryParameter("page-size", minStories);
+        //If setting to limit to a single day is checked, set the to and from dates to today.
+        if (limitToToday) {
+            String todaysDate = getTodaysDate();
+            uriBuilder.appendQueryParameter("from-date", todaysDate);
+            uriBuilder.appendQueryParameter("to-date", todaysDate);
+        }
+        uriBuilder.appendQueryParameter("api-key", apiKey);
+
+        Log.e("mainactivity", "URL built was: " + uriBuilder.toString());
+
+        return new NewsLoader(this, uriBuilder.toString());
+    }
+
+    private String getTodaysDate() {
+        DateFormat guardianDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return guardianDateFormat.format(Calendar.getInstance().getTime());
     }
 
     @Override
@@ -168,7 +209,7 @@ public class NewsListActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent settingsIntent = new Intent(this, SettingsActivity.class);
@@ -183,7 +224,7 @@ public class NewsListActivity extends AppCompatActivity
             swipeRefreshLayout.setRefreshing(true);
             isLoading = true;
             getSupportLoaderManager().restartLoader(NEWS_LOADER_ID, null, this);
-        }else {
+        } else {
             noNetworkError();
         }
 
@@ -193,11 +234,12 @@ public class NewsListActivity extends AppCompatActivity
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-               if(isLoading){restartLoader();}
-               else if(!isLoading){
-                   Toast.makeText(NewsListActivity.this, (getResources().getString(R.string.please_wait)), Toast.LENGTH_LONG).show();
+                if (!isLoading) {
+                    restartLoader();
+                } else if (isLoading) {
+                    Toast.makeText(NewsListActivity.this, (getResources().getString(R.string.please_wait)), Toast.LENGTH_LONG).show();
 
-               }
+                }
             }
         });
     }
--- app/src/main/java/com/robynandcory/goodnewseveryone/SettingsActivity.java
+++ app/src/main/java/com/robynandcory/goodnewseveryone/SettingsActivity.java
@@ -1,10 +1,11 @@
 package com.robynandcory.goodnewseveryone;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -13,11 +14,29 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragmentCompat {
+    public static class NewsPreferenceFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+
         @Override
         public void onCreatePreferences(Bundle bundle, String string) {
             //get the Preferences from teh settings_activity XML file
             addPreferencesFromResource(R.xml.settings_activity);
+
+            Preference numberOfStories = findPreference(getString(R.string.settings_number_of_stories_key));
+            updateSettingsSummary(numberOfStories);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void updateSettingsSummary(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = sharedPreferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 
